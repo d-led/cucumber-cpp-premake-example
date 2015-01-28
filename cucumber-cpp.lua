95922607aed2c0736e11f63cfb16a8fb6ad7c8e8
Dmitry Ledentsov <dmlled@yahoo.com>
2015-01-28 16:41:19 +0100

    extract cucumber-cpp build config

--- cucumber-cpp.lua
+++ cucumber-cpp.lua
@@ -1,9 +1,3 @@
-include 'premake'
-
-make_solution 'example'
-
-cucumber_cpp_root = './cucumber-cpp-premake'
-
 local function cucumber_cpp_path(dir)
 	return path.join(cucumber_cpp_root,dir)
 end
@@ -26,27 +20,3 @@ make_static_lib('cppspec',{cucumber_cpp_path 'cppspec/src/*.cpp' } )
 ----------------------------------------------------------------------------------------------------------------
 make_static_lib('cucumber-cpp-cppspec-driver', { cucumber_cpp_path 'cucumber-cpp/src/drivers/CppSpecDriver.cpp' })
 ----------------------------------------------------------------------------------------------------------------
-
-local cucumber_steps = assert( require ( cucumber_cpp_path 'recipes/cucumber-steps' ) )
-
-cucumber_steps.make_cppspec_steps ('example',{'features/**.*'}, '.')
-
-newaction {
-	trigger     = 'prepare',
-	description = 'prepare submodules',
-	execute     = function ()
-		os.execute 'git submodule update --init --recursive'
-	end	
-}
-
-newaction {
-	trigger     = 'cucumber',
-	description = 'run cucumber tests',
-	execute     = function ()
-		local util = assert( require ( cucumber_cpp_path 'recipes/util' ) )
-		util.start_cucumber {
-			start_in = '.' ,
-			executable = 'example'
-		}
-	end
-}
