d6cbfdbe2f8b29491f32f5db25b5508752e59ba2
Dmitry Ledentsov <dmlled@yahoo.com>
2015-01-29 20:28:01 +0100

    windows build

--- premake4.lua
+++ premake4.lua
@@ -1,6 +1,23 @@
 include 'premake'
 
+boost = assert(dofile 'premake/recipes/boost.lua')
+
+local OS = os.get()
+
 make_solution 'example'
+
+configuration 'windows'
+	defines { 
+		'_WIN32_WINNT=0x0501',
+		'WIN32',
+	}
+configuration '*'
+
+includedirs {
+	boost.includedirs[OS]
+}
+
+boost:set_libdirs()
 
 cucumber_cpp_root = './cucumber-cpp-premake'
 
@@ -9,6 +26,10 @@ dofile 'cucumber-cpp.lua'
 local cucumber_steps = assert( require ( path.join(cucumber_cpp_root, 'recipes/cucumber-steps') ) )
 
 cucumber_steps.make_cppspec_steps ('example',{'features/**.*'}, '.')
+
+links {
+	boost.links[OS]
+}
 
 newaction {
 	trigger     = 'prepare',
